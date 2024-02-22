7e49091e73732d096df5f5e1035f9ce457316a52
Manuel Cilurzo <manuel.cilurzo@sbb.ch>
2024-02-22 14:25:59 +0100

    lyne pfade anpassen

--- src/en/design-system/lyne/components/sbb-navigation-action/implementation/implementation.11tydata.js
+++ src/en/design-system/lyne/components/sbb-navigation-action/implementation/implementation.11tydata.js
@@ -3,7 +3,7 @@ const EleventyFetch = require("@11ty/eleventy-fetch");
 module.exports = {
     eleventyComputed: {
       implementation: data => {
-        let url = "https://raw.githubusercontent.com/lyne-design-system/lyne-components/master/src/components/navigation/navigation-action/readme.md";
+        let url = "https://raw.githubusercontent.com/lyne-design-system/lyne-components/master/src/components/navigation/navigation-link/readme.md";
         return EleventyFetch(url, {
           duration: "1d",
           type: "text"
