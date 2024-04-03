7674b57a09569acebe73802bad0c924cf8004e28
Manuel Cilurzo <manuel.cilurzo@sbb.ch>
2024-04-03 07:41:11 +0200

    lyne 0.49.0

--- src/_data/lyne/lyneversion.js
+++ src/_data/lyne/lyneversion.js
@@ -1,6 +1,6 @@
 module.exports = {
     url: 'https://raw.githubusercontent.com/lyne-design-system/lyne-components/',
-    branch: "v0.48.3",
+    branch: "v0.49.0",
     master: "master",
     path: "/src/components/",
     dok: '/readme.md'
