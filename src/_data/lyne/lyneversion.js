b6a31a7c26a232c575fc8d131582ff3f2f9d99d0
Manuel Cilurzo <manuel.cilurzo@sbb.ch>
2024-08-29 21:28:00 +0200

    redirects auf für externe seiten

--- src/_data/lyne/lyneversion.js
+++ src/_data/lyne/lyneversion.js
@@ -1,9 +1,7 @@
 module.exports = {
-    url: 'https://raw.githubusercontent.com/sbb-design-systems/lyne-components/',
-    branch: "v1.8.0",
-    master: "master",
-    path: "/src/elements/",
-    dok: '/readme.md'
-  };
-
-
+  url: 'https://raw.githubusercontent.com/sbb-design-systems/lyne-components/',
+  branch: "v1.9.0",
+  master: "master",
+  path: "/src/elements/",
+  dok: '/readme.md'
+};
