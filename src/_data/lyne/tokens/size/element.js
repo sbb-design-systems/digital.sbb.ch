8a886a1a3c03f56c8d340fd919061902e7e5900e
Manuel Cilurzo <manuel.cilurzo@sbb.ch>
2024-09-11 06:34:31 +0200

    status

--- src/_data/lyne/tokens/size/element.js
+++ src/_data/lyne/tokens/size/element.js
@@ -1,6 +1,5 @@
 const { designTokensByCategory, designTokensByPath, designTokensByType, sortByKey, groupedTokens, getNamedGroupedTokens } = require('../../../../assets/helpers/tokenHelper.js');
 const { tokens } = require('@sbb-esta/lyne-design-tokens/dist/js/sbb-tokens-raw.json');
-const durationAnimationTokens = designTokensByCategory(tokens, 'time');
 
 const sizeTokens = designTokensByPath(tokens, 'size');
 const sizeTokensElement = sizeTokens.filter((token) => token.attributes.type === 'element');
