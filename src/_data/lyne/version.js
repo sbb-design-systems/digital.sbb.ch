88997fe4140d0faef875a4582a161ddca635f2c0
Manuel Cilurzo <manuel.cilurzo@sbb.ch>
2024-10-28 19:09:35 +0100

    lyne update, fix sitemap

--- src/_data/lyne/version.js
+++ src/_data/lyne/version.js
@@ -1,6 +1,6 @@
 module.exports = {
   url: 'https://raw.githubusercontent.com/sbb-design-systems/lyne-components/',
-  branch: "v1.11.3",
+  branch: "v1.14.0",
   master: "master",
   path: "/src/elements/",
   dok: '/readme.md'
