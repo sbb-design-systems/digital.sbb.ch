8bc39b1fc6f06e19d788e60ce67879f84de3e55b
Manuel Cilurzo <manuel.cilurzo@sbb.ch>
2025-06-05 15:27:05 +0200

    lyne update

--- src/_data/lyne/version.js
+++ src/_data/lyne/version.js
@@ -1,6 +1,6 @@
 module.exports = {
   url: 'https://raw.githubusercontent.com/sbb-design-systems/lyne-components/',
-  branch: "v2.10.0",
+  branch: "v2.11.0",
   master: "master",
   path: "/src/elements/",
   dok: '/readme.md'
