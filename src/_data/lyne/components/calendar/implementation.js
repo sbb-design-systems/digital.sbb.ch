3357f7e2469c3f4b00b5adf9f71c368d5aa6355f
Manuel Cilurzo <manuel.cilurzo@sbb.ch>
2026-02-26 09:38:23 +0100

    fix calendar path

--- src/_data/lyne/components/calendar/implementation.js
+++ src/_data/lyne/components/calendar/implementation.js
@@ -1,6 +1,6 @@
 const fetchData = require('../../../../assets/helpers/fetchHelper');  
 
 module.exports = async function() {
-  const result = await fetchData("calendar");
+  const result = await fetchData("calendar/calendar");
   return result;
 };
