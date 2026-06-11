332faa64c3df8db67bad2ecd78437dc3c49406fc
Manuel Cilurzo <manuel.cilurzo@sbb.ch>
2026-06-11 15:54:19 +0200

    lyne 5.0.1 und landing page: danke angela!!

--- src/_data/lyne/version.js
+++ src/_data/lyne/version.js
@@ -1,6 +1,6 @@
 module.exports = {
   url: 'https://raw.githubusercontent.com/sbb-design-systems/lyne-components/',
-  branch: "v5.0.0",
+  branch: "v5.0.1",
   master: "master",
   path: "/src/elements/",
   pathexperimental: "/src/elements-experimental/",
