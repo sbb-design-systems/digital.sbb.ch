c39b12bef569e80b474170bf0f70ccdbba96e6fd
Beat Vollenwyder <beat.vollenwyder@sbb.ch>
2026-07-20 12:45:05 +0200

    lyne 5.3

--- src/_data/lyne/version.js
+++ src/_data/lyne/version.js
@@ -1,6 +1,6 @@
 module.exports = {
   url: 'https://raw.githubusercontent.com/sbb-design-systems/lyne-components/',
-  branch: "v5.0.1",
+  branch: "v5.3.0",
   master: "master",
   path: "/src/elements/",
   pathexperimental: "/src/elements-experimental/",
